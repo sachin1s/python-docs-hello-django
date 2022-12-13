7c73953f5d0449b1d9bc458ad1c206d9fb08a55f
sachin1s <108118324+sachin1s@users.noreply.github.com>
2022-12-13 16:46:04 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Hello  Team - IAm sachinpatel ? !")
+    return HttpResponse("Hello  Team - IAm sachinpatel  !")
